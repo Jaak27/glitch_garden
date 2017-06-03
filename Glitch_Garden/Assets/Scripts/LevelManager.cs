b862fafd952c121a53800330ce1dbf3dd63ad052
Jacques Urbany <urbanyj@hochschule-tirer.de>
2017-06-03 18:27:26 +0200

    no message

---
+++ Glitch_Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public float autoLoadNextLevelAfter;
+
+    private void Start()
+    {
+        if (autoLoadNextLevelAfter == 0)
+        {
+
+        }
+        else
+        {
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+    public void QuitRequest()
+    {
+        Debug.Log("Game quit");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void BricksDestroyed() {
+            LoadNextLevel();
+        
+    }
+
+
+
+}
